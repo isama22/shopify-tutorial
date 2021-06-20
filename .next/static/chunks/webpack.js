27a68dfe6dd2b051b328dac40b14c9ca339d595d
isama22 <emarti17@artic.edu>
2021-06-20 18:18:35 -0500

    about to test app

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5c96b20daad5a93b146e"; }
+/******/ 		__webpack_require__.h = function() { return "1f4fa375309d21d348a2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
